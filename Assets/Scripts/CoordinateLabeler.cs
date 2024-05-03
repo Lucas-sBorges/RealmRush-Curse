6ee77bf3c07ba09f54f4c3bf34690df49c5b7279
Lucas Borges <borges01.05@hotmail.com>
2024-05-03 19:54:39 -0300

    Ainda n implementou a EnemeyMovement e Waypoint

---
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{   
+    
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int(); 
+    
+    void Awake() 
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdadeObjectName();
+        }
+    }
+
+    private void DisplayCoordinates()
+    {   
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.x);
+        label.text = $"{coordinates.x} , {coordinates.y}" ;
+    }
+
+    private void UpdadeObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+
+    
+}
